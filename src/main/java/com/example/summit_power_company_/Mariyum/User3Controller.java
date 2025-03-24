97a6581a9d69f485527546bdf1126b9b7851ac0b
mariyum <mariyambintealkader@gmail.com>
2025-03-24 17:05:51 +0600

    fixed

--- src/main/java/com/example/summit_power_company_/Mariyum/User3Controller.java
+++ src/main/java/com/example/summit_power_company_/Mariyum/User3Controller.java
@@ -1,32 +1,4 @@
 package com.example.summit_power_company_.Mariyum;
 
-import javafx.event.ActionEvent;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
-
 public class User3Controller {
-    @javafx.fxml.FXML
-    private ChoiceBox departmentChoice;
-    @javafx.fxml.FXML
-    private TextField budgetAmountField;
-    @javafx.fxml.FXML
-    private TextField transactionIdField;
-    @javafx.fxml.FXML
-    private TextField billAmountField;
-
-    @javafx.fxml.FXML
-    public void handleGenerateBill(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void handleViewReport(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void handleProcessPayment(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void handleAllocateBudget(ActionEvent actionEvent) {
-    }
 }
