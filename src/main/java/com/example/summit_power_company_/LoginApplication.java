bf526bbefe6c747ac5c490364c9d6443a8aeca11
mariyum <mariyambintealkader@gmail.com>
2025-03-27 23:18:35 +0600

    Hello application to login application

--- src/main/java/com/example/summit_power_company_/LoginApplication.java
+++ src/main/java/com/example/summit_power_company_/LoginApplication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class LoginApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/summit_power_company_/Mariyum/User3_View.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("/com/example/summit_power_company_/Mariyum/User3_View.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Summit Power Company");
         stage.setScene(scene);
--- src/main/java/com/example/summit_power_company_/LoginController.java
+++ src/main/java/com/example/summit_power_company_/LoginController.java
@@ -3,7 +3,7 @@ package com.example.summit_power_company_;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LoginController {
     @FXML
     private Label welcomeText;
 
