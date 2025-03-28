459da1684f1d7fb65649bccf3f2dee243aa304be
mariyum <mariyambintealkader@gmail.com>
2025-03-28 10:46:04 +0600

    Edited Run configuration and also created fxml 0n "Login.fxml", update "module-info.java"

--- src/main/java/com/example/summit_power_company_/LoginApplication.java
+++ src/main/java/com/example/summit_power_company_/LoginApplication.java
@@ -9,8 +9,8 @@ import java.io.IOException;
 
 public class LoginApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("/com/example/summit_power_company_/Mariyum/User3_View.fxml"));
+    public void start(Stage stage) throws IOException {        ///com/example/summit_power_company_/Mariyum/User3_View.fxml
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Summit Power Company");
         stage.setScene(scene);
--- src/main/java/com/example/summit_power_company_/LoginController.java
+++ src/main/java/com/example/summit_power_company_/LoginController.java
@@ -1,14 +1,30 @@
 package com.example.summit_power_company_;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button loginButton;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    void ForgotPasswordHyperlink(ActionEvent event) {
+
     }
+
+    @FXML
+    void LoginOnActionButton(ActionEvent event) {
+
+    }
+
 }
