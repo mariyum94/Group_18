db47f0d2df2b482ad3d7ecc0b866de02b46cf9db
mariyum <mariyambintealkader@gmail.com>
2025-04-05 02:12:22 +0600

    complete the fxml of "FinanceOfficer2_View"

--- src/main/java/com/example/summit_power_company_/Mariyum/FinanceOfficer1.java
+++ src/main/java/com/example/summit_power_company_/Mariyum/FinanceOfficer1.java
@@ -1,29 +1,38 @@
 package com.example.summit_power_company_.Mariyum;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class FinanceOfficer1 {
-    @javafx.fxml.FXML
-    private TextField previousduesTextField;
-    @javafx.fxml.FXML
-    private TextField totalunitsconsumedTextField;
-    @javafx.fxml.FXML
+
+    @FXML
     private TextField CustomeridTextfield;
 
-    @javafx.fxml.FXML
-    public void ReturnHomeOnActionButton(ActionEvent actionEvent) {
+    @FXML
+    private TextField previousduesTextField;
+
+    @FXML
+    private TextField totalunitsconsumedTextField;
+
+    @FXML
+    void FetchUsageOnActionButton(ActionEvent event) {
+
     }
 
-    @javafx.fxml.FXML
-    public void SavePrintOnActionButton(ActionEvent actionEvent) {
+    @FXML
+    void GenerateBill(ActionEvent event) {
+
     }
 
-    @javafx.fxml.FXML
-    public void GenerateBill(ActionEvent actionEvent) {
+    @FXML
+    void ReturnHomeOnActionButton(ActionEvent event) {
+
     }
 
-    @javafx.fxml.FXML
-    public void FetchUsageOnActionButton(ActionEvent actionEvent) {
+    @FXML
+    void SavePrintOnActionButton(ActionEvent event) {
+
     }
+
 }
--- src/main/java/com/example/summit_power_company_/Mariyum/FinanceOfficer2.java
+++ src/main/java/com/example/summit_power_company_/Mariyum/FinanceOfficer2.java
@@ -1,4 +1,39 @@
 package com.example.summit_power_company_.Mariyum;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class FinanceOfficer2 {
+
+    @FXML
+    private TextField invoiceNumberTextField;
+
+    @FXML
+    private Label paymentStatusLabel;
+
+    @FXML
+    private Label successMessageLabel;
+
+    @FXML
+    void FetchPaymentStatusOnActionButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MarkAsPaidOnActionButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void PaymentProcessingOnActionButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ReturnHomeOnActionButton(ActionEvent event) {
+
+    }
+
 }
