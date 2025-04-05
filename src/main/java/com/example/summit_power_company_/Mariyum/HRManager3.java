6ec2bbcc5b61488ab7790d96ac699820ecd85d98
mariyum <mariyambintealkader@gmail.com>
2025-04-05 23:56:37 +0600

    create HR manager3 scene

--- src/main/java/com/example/summit_power_company_/Mariyum/HRManager3.java
+++ src/main/java/com/example/summit_power_company_/Mariyum/HRManager3.java
@@ -1,4 +1,37 @@
 package com.example.summit_power_company_.Mariyum;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class HRManager3 {
+
+    @FXML
+    private TableColumn<?, ?> employeeColumn;
+
+    @FXML
+    private TableView<?> leaveRequestTable;
+
+    @FXML
+    private TableColumn<?, ?> leaveTypeColumn;
+
+    @FXML
+    private TableColumn<?, ?> statusColumn;
+
+    @FXML
+    void ReturnHomeOnActionButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void approveLeaveOnActionButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void denyleaveOnActionButton(ActionEvent event) {
+
+    }
+
 }
