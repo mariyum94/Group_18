ad6705fce94a8403f6c4e87dc5d947a45c93e6c0
mariyum <mariyambintealkader@gmail.com>
2025-04-07 16:01:10 +0600

    update "ReturnHome" button for all class

--- src/main/java/com/example/summit_power_company_/Mariyum/HRManager4.java
+++ src/main/java/com/example/summit_power_company_/Mariyum/HRManager4.java
@@ -2,8 +2,13 @@ package com.example.summit_power_company_.Mariyum;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class HRManager4 {
 
@@ -18,7 +23,13 @@ public class HRManager4 {
     }
     @FXML
     void ReturnHomeOnActionButton(ActionEvent event) {
-
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("HRManagerDashboard.fxml"));
+            Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
