6841d6429cfbf5e39fd171904305416621d32ae5
mariyum <mariyambintealkader@gmail.com>
2025-04-11 16:33:33 +0600

    update LoginController and facing some errors to entering dashboard

--- src/main/java/com/example/summit_power_company_/LoginController.java
+++ src/main/java/com/example/summit_power_company_/LoginController.java
@@ -1,43 +1,34 @@
 package com.example.summit_power_company_;
 
+import com.example.summit_power_company_.Mariyum.SceneSwitcher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+
+
+import java.io.IOException;
 
 public class LoginController {
+    @FXML
+    private PasswordField passwordTextField;
+    @FXML
+    private Label errorLabel;
+    @FXML
+    private TextField usernameTextField;
 
     @FXML
-    private Button loginButton;
-
-    @FXML
-    private PasswordField passwordField;
-
-    @FXML
-    private TextField usernameField;
-    @FXML
-    private Label errormassage;
-
-    @FXML
-    void ForgotPasswordHyperlink(ActionEvent event) {
-
+    public void initialize() {
     }
 
     @FXML
-    void LoginOnActionButton(ActionEvent event) {
-
-            String username = usernameField.getText();
-            String password = passwordField.getText();
-
-            if (username.equals("mariyum") && password.equals("1234")) {
-                errormassage.setText("Log in successful");
-            }
-            else {
-                errormassage.setText("  Incorrect username or password  ");
-            }
-
+    public void login(ActionEvent actionEvent) throws IOException {
+        String username = usernameTextField.getText();
+        String password = passwordTextField.getText();
+        if ("mariyum".equals(username) && "mariyum123".equals(password)) {
+            errorLabel.setText("Log in successful");
+            SceneSwitcher.switchTo("com.example.summit_power_company_.Mariyum.FinanceOfficerDashboard");
+        } else {
+            errorLabel.setText("Incorrect username or password");
+        }
     }
-
 }
--- src/main/java/com/example/summit_power_company_/Mariyum/FinanceOfficer2.java
+++ src/main/java/com/example/summit_power_company_/Mariyum/FinanceOfficer2.java
@@ -2,13 +2,9 @@ package com.example.summit_power_company_.Mariyum;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
