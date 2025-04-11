816cf401e757d7c9f4e3fd82233da68a3da6c4dc
mariyum <mariyambintealkader@gmail.com>
2025-04-11 13:25:11 +0600

    added "SceneSwitcher" class

--- src/main/java/com/example/summit_power_company_/Mariyum/FinanceOfficer2.java
+++ src/main/java/com/example/summit_power_company_/Mariyum/FinanceOfficer2.java
@@ -22,6 +22,7 @@ public class FinanceOfficer2 {
 
     @FXML
     private Label successMessageLabel;
+    private ActionEvent actionEvent;
 
     @FXML
     void FetchPaymentStatusOnActionButton(ActionEvent event) {
@@ -34,16 +35,7 @@ public class FinanceOfficer2 {
     }
 
     @FXML
-    void ReturnHomeOnActionButton(ActionEvent event) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("FinanceOfficerDashboard_View.fxml"));
-            Scene scene = new Scene(root);
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    void ReturnHomeOnActionButton(ActionEvent event) throws IOException {
+        SceneSwitcher.switchTo("FinanceOfficerDashboard_View.fxml", actionEvent);
     }
-
 }
