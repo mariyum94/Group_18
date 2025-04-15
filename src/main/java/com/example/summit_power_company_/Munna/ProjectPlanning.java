7e63bd808a055f3f7866318cb38de5e44e03ff5c
Naimur Rahman Munna <naimurrahmanmunna22@gmail.com>
2025-04-15 23:16:47 +0600

    Project Planning & Fxml

---
+++ src/main/java/com/example/summit_power_company_/Munna/ProjectPlanning.java
@@ -0,0 +1,38 @@
+package com.example.summit_power_company_.Munna;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+
+import java.awt.*;
+
+public class ProjectPlanning
+{
+    @javafx.fxml.FXML
+    private DatePicker Sdate;
+    @javafx.fxml.FXML
+    private TextField Description;
+    @javafx.fxml.FXML
+    private DatePicker Edate;
+    @javafx.fxml.FXML
+    private TextField Resources;
+    @javafx.fxml.FXML
+    private TextField MemberName;
+    @javafx.fxml.FXML
+    private TextField Name;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void Save(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void NewProject(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void GoToHome(ActionEvent actionEvent) {
+    }
+}
