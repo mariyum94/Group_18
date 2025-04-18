7bc8c850da1b1c124063f5ceb160ba14c2035d4c
Naimur Rahman Munna <naimurrahmanmunna22@gmail.com>
2025-04-18 13:23:22 +0600

    Monitoring and Controlling,It's fxml..ResourceAllocation & it's Fxml

---
+++ src/main/java/com/example/summit_power_company_/Munna/ResourceAllocation.java
@@ -0,0 +1,18 @@
+package com.example.summit_power_company_.Munna;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+
+public class ResourceAllocation
+{
+    @javafx.fxml.FXML
+    private ListView List;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void ResourceAllocation(ActionEvent actionEvent) {
+    }
+}
