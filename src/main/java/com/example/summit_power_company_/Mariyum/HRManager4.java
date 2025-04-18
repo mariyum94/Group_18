48bf2acff196586f12fb3a5af0e7ab851e43edcc
mariyum <mariyambintealkader@gmail.com>
2025-04-18 14:47:44 +0600

    update " FinanceOfficer7_View.fxml " and its controller

--- src/main/java/com/example/summit_power_company_/Mariyum/HRManager4.java
+++ src/main/java/com/example/summit_power_company_/Mariyum/HRManager4.java
@@ -2,15 +2,9 @@ package com.example.summit_power_company_.Mariyum;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HRManager4 {
