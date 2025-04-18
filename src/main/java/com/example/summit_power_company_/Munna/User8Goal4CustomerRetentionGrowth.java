ce58dcd5a3b6e4aeb440f77fe868296633f81e05
Naimur Rahman Munna <naimurrahmanmunna22@gmail.com>
2025-04-18 20:52:03 +0600

    User 8 java class and Fxml

---
+++ src/main/java/com/example/summit_power_company_/Munna/User8Goal4CustomerRetentionGrowth.java
@@ -0,0 +1,7 @@
+package com.example.summit_power_company_.Munna;
+
+public class User8Goal4CustomerRetentionGrowth
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/com/example/summit_power_company_/Munna/User8Goal7IdentifyNewCustomers.java
@@ -0,0 +1,7 @@
+package com.example.summit_power_company_.Munna;
+
+public class User8Goal7IdentifyNewCustomers
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
