56f0650a018432f93ee1ed937f13154e8f191404
Naimur Rahman Munna <naimurrahmanmunna22@gmail.com>
2025-04-18 13:59:36 +0600

    Track Daily power Generation & fxml

---
+++ src/main/java/com/example/summit_power_company_/Munna/TrackDailyPowerGeneration.java
@@ -0,0 +1,36 @@
+package com.example.summit_power_company_.Munna;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class TrackDailyPowerGeneration
+{
+    @javafx.fxml.FXML
+    private ChoiceBox Generation;
+    @javafx.fxml.FXML
+    private TextField Report;
+    @javafx.fxml.FXML
+    private ListView list;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void Add(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void Gotohome(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void Export(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void Compare(ActionEvent actionEvent) {
+    }
+}
