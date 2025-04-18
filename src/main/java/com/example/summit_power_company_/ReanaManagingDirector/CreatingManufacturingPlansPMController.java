f5d67315874e4de8a19df924a0e6ba3953728f06
ReanaAhmed <1910961@iub.edu.bd>
2025-04-18 19:07:34 +0600

    manufacturing file handling

--- src/main/java/com/example/summit_power_company_/ReanaManagingDirector/CreatingManufacturingPlansPMController.java
+++ src/main/java/com/example/summit_power_company_/ReanaManagingDirector/CreatingManufacturingPlansPMController.java
@@ -1,7 +1,53 @@
 package com.example.summit_power_company_.ReanaManagingDirector;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class CreatingManufacturingPlansPMController
 {
     @javafx.fxml.FXML
+    private TextField budgetTextField;
+    @javafx.fxml.FXML
+    private TableColumn manpowerTColumn;
+    @javafx.fxml.FXML
+    private TableColumn nameColumn;
+    @javafx.fxml.FXML
+    private TextField materialQuantityTextField;
+    @javafx.fxml.FXML
+    private TextField materialNameTextField;
+    @javafx.fxml.FXML
+    private TableView tableView;
+    @javafx.fxml.FXML
+    private TextField manpowerTextField;
+    @javafx.fxml.FXML
+    private TableColumn quantityTColumn;
+    @javafx.fxml.FXML
+    private Label messageLabel;
+    @javafx.fxml.FXML
+    private Label msgLabel;
+    @javafx.fxml.FXML
+    private TableColumn budgetTC;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void addProjectInfoButtonOnClicked(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goToActivitiesButtonOnclicked(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sendMsgButtonOnClicked(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void editProjectInfoButtonOnClicked(ActionEvent actionEvent) {
+    }
+}
