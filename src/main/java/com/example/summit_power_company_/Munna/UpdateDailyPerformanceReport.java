5c76ad16fa6f21e2d2f551954d2113709d70e1c5
Naimur Rahman Munna <naimurrahmanmunna22@gmail.com>
2025-04-18 20:37:54 +0600

    Update Daily Performance Report and Fxml

---
+++ src/main/java/com/example/summit_power_company_/Munna/UpdateDailyPerformanceReport.java
@@ -0,0 +1,22 @@
+package com.example.summit_power_company_.Munna;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class UpdateDailyPerformanceReport {
+    @javafx.fxml.FXML
+    private TextField Report;
+    @javafx.fxml.FXML
+    private TextField Attachfile;
+    @javafx.fxml.FXML
+    private ListView list;
+
+    @javafx.fxml.FXML
+    public void Gotohomepage(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void Submit(ActionEvent actionEvent) {
+    }
+}
